565af1be987861d12a19cae570b5745904e5951c
Sergey A. Shestakov <gnoul@gnoul.com>
2014-08-08 11:37:21 +0900

    some changes

--- plot1.R
+++ plot1.R
@@ -1,7 +1,9 @@
+#Loading data
 pcb <- read.table("./data/household_power_consumption.txt", head=TRUE, sep =";", na.string="?", colClasses=c("character", "character", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric"))
+#Subsetting
 pcb <- subset(pcb, Date=="1/2/2007"| Date=="2/2/2007")
-#pcb$Date <- as.Date(pcb$Date, "%d/%m/%Y")
 gp <- pcb$Global_active_power
+#Create Plot
 png("plot1.png", width = 480, height = 480, unit = "px", bg = "transparent")
 hist(gp, main = "Global Active Power", col = "red", xlab = "Global Active Power (kilowatts)", ylab = "Frequency")
 dev.off()
--- plot2.R
+++ plot2.R
@@ -1,11 +1,14 @@
+#Loading data
 pcb <- read.table("./data/household_power_consumption.txt", head=TRUE, sep =";", na.string="?", colClasses=c("character", "character", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric"))
+#Subsetting
 pcb <- subset(pcb, Date=="1/2/2007"| Date=="2/2/2007")
-#pcb$Date <- as.Date(pcb$Date, "%d/%m/%Y")
+#Add date-time column
 pcb$datetime <- paste(pcb$Date,pcb$Time)
 pcb$datetime <- strptime(pcb$datetime, "%d/%m/%Y %H:%M:%S")
-#gp <- pcb$Global_active_power
+#Creating plot device
 png("plot2.png", width = 480, height = 480, unit = "px", bg = "transparent")
-#
+#Draw plot
 with(pcb, plot(datetime, Global_active_power, type="l",ann=FALSE))
+#Add title
 title(ylab="Global Active Power (kilowatts)")
 dev.off()
